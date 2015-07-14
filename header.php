6b0311a3f756c16ef095daf57803e89e284977af
Nathan Beers <nbeers22@gmail.com>
2015-07-14 11:35:24 -0400

    Initial Commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="container" id="main-container">
+		<nav class="navbar navbar-inverse navbar-static-top">
+		  <div class="container">
+		    ...
+		  </div>
+		</nav>
+	
+	
+
+</body>
+</html>
+
